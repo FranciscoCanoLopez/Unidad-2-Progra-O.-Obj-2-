a1a94cfe809698913fe77d053f5efeaf08641c5d
Francisco Cano <volublecoffe@gmail.com>
2024-02-19 22:15:43 -0600

    commint

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Vehicles;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car("Toyota", "Corolla", 2022, "Red");
+
+        myCar.ShowInformation();
+
+        Console.WriteLine("");
+
+        myCar.Accelerate(50);
+
+        myCar.Brake(20);
+
+        Console.WriteLine("");
+
+        myCar.ShowInformation();
+    }
+}
---
+++ vehicles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private int year;
+        private string color;
+        private int speed;
+
+        public Car(string brand, string model, int year, string color)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+            this.speed = 0;
+        }
+
+        public void Accelerate(int increment)
+        {
+            this.speed += increment;
+            Console.WriteLine("The car accelerated to " + this.speed + " km/h");
+        }
+
+        public void Brake(int decrement)
+        {
+            this.speed -= decrement;
+            if (this.speed < 0)
+                this.speed = 0;
+            Console.WriteLine("The car slowed down to " + this.speed + " km/h");
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine("Brand: " + this.brand);
+            Console.WriteLine("Model: " + this.model);
+            Console.WriteLine("Year: " + this.year);
+            Console.WriteLine("Color: " + this.color);
+            Console.WriteLine("Current Speed: " + this.speed + " km/h");
+        }
+    }
+}
